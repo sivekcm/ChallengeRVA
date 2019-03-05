b4444926e2c694155b3f34cb5ed617c4498f4ef7
Shradda <shradda.adh@gmail.com>
2019-03-05 13:30:45 -0500

    first commit

---
+++ Application/app/src/main/java/com/example/challengerva/Coach.java
@@ -0,0 +1,19 @@
+package com.example.challengerva;
+
+public class Coach {
+    private String username;
+    private String password;
+
+    public void setUsername(String name){
+        this.username = name;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setMaster(String pass){
+        this.password = pass;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
