f2b3352df86a445bb755845bbb98ded3d9bac562
Adrienne Serra <serraaj2@vcu.edu>
2019-03-08 22:51:38 -0500

    Adrienne 3/8

--- Application/app/src/main/java/com/example/challengerva/MainActivity.java
+++ Application/app/src/main/java/com/example/challengerva/MainActivity.java
@@ -13,11 +13,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Login Button
         Button loginActivityLoginBtn = (Button) findViewById(R.id.loginActivityLoginButton);
         loginActivityLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                // Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+            }
+        });
+
+        //Forgot Username Button
+        Button forgotUsernameActivityBtn = (Button) findViewById(R.id.loginActivityForgotUsernameButton);
+        forgotUsernameActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent forgotUsernameIntent = new Intent(getApplicationContext(), ForgotUsernameActivity.class);
+                startActivity(forgotUsernameIntent);
+            }
+        });
+
+        //Forgot Password Button
+        Button forgotPasswordActivityBtn = (Button) findViewById(R.id.loginActivityForgotPasswordButton);
+        forgotPasswordActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent forgotPasswordIntent = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
+               startActivity(forgotPasswordIntent);
             }
         });
 
