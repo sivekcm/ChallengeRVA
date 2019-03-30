54f8a3e5eecbb95f0a75df9064ea1112357c7ff4
Shradda <shradda.adh@gmail.com>
2019-03-29 20:32:23 -0400

    team selection

--- Application/app/src/main/java/com/example/challengerva/AthleteCreateTeamActivity.java
+++ Application/app/src/main/java/com/example/challengerva/AthleteCreateTeamActivity.java
@@ -3,6 +3,7 @@ package com.example.challengerva;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.NumberPicker;
@@ -14,6 +15,7 @@ public class AthleteCreateTeamActivity extends AppCompatActivity {
     EditText teamNameEditTxt;
     NumberPicker teamNumberPicker;
     TextView createTeamTxtView;
+    TextView challengeNameTxtView;
     Button createTeamBtn;
     ArrayList<User>teamMembers;
 
@@ -25,14 +27,16 @@ public class AthleteCreateTeamActivity extends AppCompatActivity {
         Intent intent = getIntent();
         final User athlete = intent.getParcelableExtra("User Object");
 
+
         teamNameEditTxt = (EditText) findViewById(R.id.teamNameEditTxt);
-        teamNumberPicker = (NumberPicker) findViewById(R.id.teamNumberPicker);
         createTeamTxtView = (TextView)findViewById(R.id.createTeamTxtView);
+        challengeNameTxtView = (TextView)findViewById(R.id.challengeNameTxtView);
+
+        //challengeNameTxtView.setText(Challenge);
         createTeamBtn = (Button)findViewById(R.id.createTeamBtn);
 
         //create challenge and get the min and max number for the team
-
-        teamNumberPicker.setMinValue(2);
+        teamNumberPicker.setMinValue(2); //should be according to the challenge
         teamNumberPicker.setMaxValue(100);
         teamNumberPicker.setWrapSelectorWheel(false);
 
