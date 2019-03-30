63e8f788bfb9ff20a08eb3a55e66dc0d20ca3a8c
Shradda <shradda.adh@gmail.com>
2019-03-30 15:37:28 -0400

    added team adapter

--- Application/app/src/main/java/com/example/challengerva/AthleteTeamSelectionActivity.java
+++ Application/app/src/main/java/com/example/challengerva/AthleteTeamSelectionActivity.java
@@ -1,14 +1,25 @@
 package com.example.challengerva;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AthleteTeamSelectionActivity extends AppCompatActivity {
     TextView challengeNameTxtView;
-    TextView selectTeamTxtView;
     RecyclerView teamSelectionRecyclerView;
+    TeamAdapter adapter;
+    DBHelper db = new DBHelper(this);
+
+    Cursor teamData;
+    Cursor cursor;
+    Button currentBtn;
+    Button previousBtn;
+    User user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,18 @@ public class AthleteTeamSelectionActivity extends AppCompatActivity {
         setContentView(R.layout.athlete_team_selection);
 
         challengeNameTxtView = (TextView)findViewById(R.id.challengeNameTxtView);
-        selectTeamTxtView = (TextView)findViewById(R.id.challengeNameTxtView);
         teamSelectionRecyclerView = findViewById(R.id.teamSelectionRecyclerView);
+
+        String name = cursor.getString(1);
+        challengeNameTxtView.setText(name);
+
+        //showResults(db.getTeamData();
+    }
+
+    public void showResults(Cursor cursor)
+    {
+        teamSelectionRecyclerView.setLayoutManager(new LinearLayoutManager(AthleteTeamSelectionActivity.this));
+        adapter = new TeamAdapter(AthleteTeamSelectionActivity.this, cursor);
+        teamSelectionRecyclerView.swapAdapter(adapter,false);
     }
 }
